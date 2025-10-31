5587115195a41c731eb19f6352b2d96522541647
ImaanCummings <imaancummingss13@gmail.com>
2025-10-31 09:20:19 +0200

    first commit

---
+++ operationals.js
@@ -0,0 +1,13 @@
+let score = 70
+
+const passingGrade = 60
+
+if (score === 100) {
+    console.log("Perfect Score!")
+}
+else if (score >= passingGrade) {
+    console.log("Passed")
+}
+else if (score < passingGrade) {
+    console.log("Failed")
+}
